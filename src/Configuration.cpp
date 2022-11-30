c59ccd7ebdf59dcc78927f1f27376fe6501b5aac
Guillermo Serrahima <gserrahima@verbio.com>
2022-11-30 09:38:59 +0100

    Added grpc un/secure channel toggle Better type parsing for arguments

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -17,13 +17,14 @@ void Configuration::parse(int argc, char **argv) {
 
     cxxopts::Options options(argv[0], "Verbio Technlogies S.L. - Speech Center client example");
     options.set_width(180).allow_unrecognised_options().add_options()
-            ("a", "Path to a .wav audio in 8kHz and PCM16 encoding to use for the recognition", cxxopts::value(audioPath), "file")
-            ("g", "Path to the Grammar ABNF file to use for the recognition", cxxopts::value(grammarPath), "file")
-            ("T", "Topic to use for the recognition when a grammar is not provided. Must be GENERIC | BANKING | TELCO | INSURANCE", cxxopts::value(topic)->default_value(topic))
-            ("l", "Language to use for the recognition: es-ES, en-US or pt-BR.", cxxopts::value(language)->default_value(language))
-            ("s", "Sample rate for the audio recognition: 16000.", cxxopts::value(sampleRate)->default_value(sampleRate))
-            ("t", "Path to the authentication token file", cxxopts::value(tokenPath))
-            ("H", "URL of the Host or server trying to reach", cxxopts::value(host)->default_value(host))
+            ("a,audio", "Path to a .wav audio in 8kHz and PCM16 encoding to use for the recognition", cxxopts::value(audioPath), "file")
+            ("g,grammar", "Path to the Grammar ABNF file to use for the recognition", cxxopts::value(grammarPath), "file")
+            ("T,topic", "Topic to use for the recognition when a grammar is not provided. Must be GENERIC | BANKING | TELCO | INSURANCE", cxxopts::value(topic)->default_value(topic))
+            ("l,language", "Language to use for the recognition: es-ES, en-US or pt-BR.", cxxopts::value(language)->default_value(language))
+            ("s,sample-rate", "Sample rate for the audio recognition: 8000, 16000.", cxxopts::value<uint32_t>(sampleRate))
+            ("t,token", "Path to the authentication token file", cxxopts::value(tokenPath))
+            ("H,host", "URL of the Host or server trying to reach", cxxopts::value(host)->default_value(host))
+            ("S,not-secure", "Toggle for non-secure GRPC connections", cxxopts::value<bool>(notSecure)->default_value("false"))
             ("h,help", "this help message");
     auto parsedOptions = options.parse(argc, argv);
 
@@ -47,10 +48,6 @@ std::string Configuration::getLanguage() const {
     return language;
 }
 
-std::string Configuration::getSampleRate() const {
-    return sampleRate;
-}
-
 std::string Configuration::getTokenPath() const {
     return tokenPath;
 }
@@ -63,6 +60,10 @@ std::string Configuration::getTopic() const {
     return topic;
 }
 
+uint32_t Configuration::getSampleRate() const {
+    return sampleRate;
+}
 
-
-
+bool Configuration::getNotSecure() const {
+    return notSecure;
+}
--- src/RecognitionClient.cpp
+++ src/RecognitionClient.cpp
@@ -59,13 +59,24 @@ std::shared_ptr<grpc::Channel>
 RecognitionClient::createChannel(const Configuration &configuration) {
     auto jwt = readFileContent(configuration.getTokenPath());
     std::cout << "TOKEN: -" << jwt << "-" << std::endl;
-    channel = grpc::CreateChannel(configuration.getHost(),
-              grpc::CompositeChannelCredentials(
-                    grpc::SslCredentials(grpc::SslCredentialsOptions()),
-                    grpc::AccessTokenCredentials(jwt)
-            )
-    );
-    context.set_credentials(grpc::AccessTokenCredentials(jwt));
+
+    // GRPC Non/secure toggle
+    if(configuration.getNotSecure()) { // Not secure:
+        channel = grpc::CreateChannel(configuration.getHost(),
+                                      grpc::InsecureChannelCredentials()
+        );
+        context.AddMetadata("authorization", "Bearer" + jwt);
+    }
+    else { // Secure
+        channel = grpc::CreateChannel(configuration.getHost(),
+                                      grpc::CompositeChannelCredentials(
+                                              grpc::SslCredentials(grpc::SslCredentialsOptions()),
+                                              grpc::AccessTokenCredentials(jwt)
+                                      )
+        );
+        context.set_credentials(grpc::AccessTokenCredentials(jwt));
+    }
+
     channel->WaitForConnected(std::chrono::system_clock::now() + std::chrono::seconds(5));
     if (channel->GetState(false) != GRPC_CHANNEL_READY)
         if (!channel->WaitForStateChange(GRPC_CHANNEL_READY,
@@ -173,13 +184,11 @@ RecognitionClient::buildRecognitionParameters(const Configuration &configuration
 }
 
 std::unique_ptr<speechcenter::recognizer::v1::PCM>
-RecognitionClient::buildPCM(const std::string &sampleRate) {
+RecognitionClient::buildPCM(const uint32_t &sampleRate) {
     std::unique_ptr<speechcenter::recognizer::v1::PCM> pcm (new speechcenter::recognizer::v1::PCM());
     uint32_t sampleRateHz;
-    if(sampleRate == "16000" || sampleRate.empty())
-        sampleRateHz = 16000;
-    else
-        throw UnsupportedSampleRate(sampleRate);
+    if(sampleRate != 16000 && sampleRate != 8000)
+        throw UnsupportedSampleRate(std::to_string(sampleRate));
     pcm->set_sample_rate_hz(sampleRateHz);
     return pcm;
 }
@@ -194,13 +203,13 @@ RecognitionClient::buildRecognitionResource(const Configuration &configuration) 
 
 speechcenter::recognizer::v1::RecognitionResource_Model RecognitionClient::convertTopicModel(const std::string &modelName) {
     speechcenter::recognizer::v1::RecognitionResource_Model model;
-    if (modelName == "generic")
+    if (modelName == "generic" || modelName == "GENERIC")
         model = speechcenter::recognizer::v1::RecognitionResource_Model_GENERIC;
-    else if (modelName == "banking")
+    else if (modelName == "banking" || modelName == "BANKING")
         model = speechcenter::recognizer::v1::RecognitionResource_Model_BANKING;
-    else if (modelName == "telco")
+    else if (modelName == "telco" || modelName == "TELCO")
         model = speechcenter::recognizer::v1::RecognitionResource_Model_TELCO;
-    else if (modelName == "insurance")
+    else if (modelName == "insurance" || modelName == "INSURANCE")
         model = speechcenter::recognizer::v1::RecognitionResource_Model_INSURANCE;
     else
         throw UnknownTopicModel(modelName);
