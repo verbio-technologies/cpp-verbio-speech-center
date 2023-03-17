2cb1087da6a164deb0210c8f15ac144cc5aeefd1
Jordi Adell <jadell@verbio.com>
2023-03-17 09:27:07 +0100

    changed Audio class to separate reponsibilities
    One class to manage the raw audio and another one to manage the
    audio files.

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -4,17 +4,36 @@
 
 #include "Audio.h"
 
+#include "gRpcExceptions.h"
+
 #include "logger.h"
 #include "sndfile.hh"
 
-Audio::Audio(const std::string &audioPath) {
-    SndfileHandle sndfileHandle(audioPath);
-    length = sndfileHandle.frames();
-    data = new int16_t[length];
-    length = sndfileHandle.read(data, length);
-    INFO("Read {} samples with {} bytes per sample", sndfileHandle.frames(), getBytesPerSamples());
+Audio::Audio(const int16_t* const _data, int _samplingRate, int lengthInFrames) : length(lengthInFrames), samplingRate(_samplingRate) {
+    data = std::make_unique<int16_t[]>(length);
+    std::copy_n(_data,  lengthInFrames, data.get());
 }
 
-Audio::~Audio() {
-    delete data;
+Audio::~Audio() =default;
+
+
+AudioFile::AudioFile(const std::string &audioPath) : audio(readAudioFromFile(audioPath)) {
+
 }
+#include <iostream>
+Audio AudioFile::readAudioFromFile(const std::string &audioPath) {
+    SndfileHandle sndfileHandle(audioPath);
+    auto length = sndfileHandle.frames();
+    auto data = new int16_t[length];
+    length = sndfileHandle.read(data, length);
+    auto samplingRate  = sndfileHandle.samplerate();
+    if (! sndfileHandle.formatCheck(SF_FORMAT_PCM_16 | SF_FORMAT_WAV, sndfileHandle.channels(), samplingRate))
+        throw GrpcException("Unsupported file audio format");
+    INFO("Read {} samples with {} bytes per sample", length, sizeof(data));
+    return Audio(data, samplingRate, length);
+}
+
+AudioFile::~AudioFile() = default;
+
+
+
